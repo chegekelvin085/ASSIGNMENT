d59d90cdbec00d899da6932b7b9bfa885d149d2b
chegekelvin085 <chegekelvin085@gmail.com>
2025-09-25 11:51:43 +0300

    Add files via upload

---
+++ Assignment.c
@@ -0,0 +1,21 @@
+//simple c program (hello world),
+/*
+Name:kelvin
+Reg:26144
+Description:Hello world program
+*/
+
+#include<stdio.h>//
+
+//main function
+int main(){
+        printf("hello world:\n");
+         printf("computer sytem and organization\n");
+         printf("Analogue electronics\n ");
+         printf("Fundamentals of ICT\n");
+       printf("Essentialof accounting\n");
+       printf("communication skills\n ");
+       printf("Mathematics for science\n");
+       printf("Introduction to programming  and Alogarithms\n");
+         return 0;
+ }        
