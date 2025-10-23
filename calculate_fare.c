e5e370f1f7c140bd4725f417db19424e9e27d661
chegekelvin085 <chegekelvin085@gmail.com>
2025-10-23 23:05:04 +0300

    Add files via upload

---
+++ calculate_fare.c
@@ -0,0 +1,23 @@
+/*
+NAME:KELVIN KANGERI CHEGE
+REG NO:CT100/G/26144/25
+DESCRIPTION:calculate fare
+*/
+#include <stdio.h>
+
+// Function to calculate total fare
+int calculateFare(int distance) {
+    int fare = distance * 50;
+    return fare;
+}
+
+int main() {
+    int distance;
+    printf("Enter distance traveled (in km): ");
+    scanf("%d", &distance);
+
+    int totalFare = calculateFare(distance);
+    printf("Total fare = KSh. %d\n", totalFare);
+
+    return 0;
+}
